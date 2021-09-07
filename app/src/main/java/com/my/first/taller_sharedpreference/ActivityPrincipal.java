6a29ed55dcf2e70f6d56d65d6f977bf9b17d4314
Wilson-CV <carpiowilson00@outlook.com>
2021-09-07 08:26:32 -0500

    Arreglar rutas URL

--- app/src/main/java/com/my/first/taller_sharedpreference/ActivityPrincipal.java
+++ app/src/main/java/com/my/first/taller_sharedpreference/ActivityPrincipal.java
@@ -62,9 +62,11 @@ public class ActivityPrincipal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        SharedPreferences preferences=getSharedPreferences("preferenciasLogin", Context.MODE_PRIVATE);
 
-        buscarUsuario("http://192.168.100.7/ProyectoAndroid2/buscar_usuario.php?email="+preferences.getString("usuario","")+"");
+        SharedPreferences preferences=getSharedPreferences("preferenciasLogin", Context.MODE_PRIVATE);
+        //preferences.edit().clear().commit();
+
+        buscarUsuario("http://192.168.1.3/ProyectoAndroid2/buscar_usuario.php?email="+preferences.getString("usuario","")+"");
         //navigationView = (NavigationView) findViewById(R.id.nav_host_fragment_content_activity_principal);
 
 

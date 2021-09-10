a2cbef12ee7e3dd869c431321504999c031c072c
Wilson-CV <carpiowilson00@outlook.com>
2021-09-09 23:45:43 -0500

    Mostrar las noticias funcional

--- app/src/main/java/com/my/first/taller_sharedpreference/ui/home/HomeFragment.java
+++ app/src/main/java/com/my/first/taller_sharedpreference/ui/home/HomeFragment.java
@@ -5,23 +5,46 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.my.first.taller_sharedpreference.ActivityPrincipal;
+import com.my.first.taller_sharedpreference.Articles;
+import com.my.first.taller_sharedpreference.NewsModal;
+import com.my.first.taller_sharedpreference.NewsRVAdapter;
 import com.my.first.taller_sharedpreference.R;
+import com.my.first.taller_sharedpreference.RetrofitAPI;
 import com.my.first.taller_sharedpreference.databinding.FragmentHomeBinding;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
+
+    //Variables que se usaran para agregar las noticias
+    private RecyclerView newsRV;
+    private ProgressBar loadingPB;
+    private ArrayList<Articles> articlesArrayList;
+    private NewsRVAdapter newsRVAdapter;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -30,15 +53,14 @@ public class HomeFragment extends Fragment {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
         //final TextView textView = binding.textHome; Este textview ya no existe
+
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 //textView.setText(s); Este textview ya no existe
             }
         });
-
 
         final ActivityPrincipal ma = (ActivityPrincipal) getActivity();
         SharedPreferences sp = ma.getSharedPreferences("SP", ma.MODE_PRIVATE);
@@ -67,7 +89,55 @@ public class HomeFragment extends Fragment {
             }
         });
 
+        //Codigo para agregar las noticias
+        newsRV = root.findViewById(R.id.idRVNews);
+        loadingPB = root.findViewById(R.id.idPBLoading);
+        articlesArrayList = new ArrayList<>();
+        newsRVAdapter = new NewsRVAdapter(articlesArrayList,this.getContext());
+        newsRV.setLayoutManager(new LinearLayoutManager(this.getContext()));
+        newsRV.setAdapter(newsRVAdapter);
+        getNews();
+        newsRVAdapter.notifyDataSetChanged();
+
+        //Fin de codigo para agregar las noticias
+
         return root;
+    }
+
+    private void getNews()
+    {
+        loadingPB.setVisibility(View.VISIBLE);
+        articlesArrayList.clear();
+        String url = "https://newsapi.org/v2/top-headlines?country=mx&category=science&language=es&apikey=8df9385425ca463f9ddaf899a09aa2f2";
+        String BASEurl = "https://newsapi.org";
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASEurl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        RetrofitAPI retrofitAPI = retrofit.create(RetrofitAPI.class);
+        Call<NewsModal> call;
+        call = retrofitAPI.getAllNews(url);
+
+        call.enqueue(new Callback<NewsModal>() {
+            @Override
+            public void onResponse(Call<NewsModal> call, Response<NewsModal> response) {
+                NewsModal newsModal = response.body();
+                loadingPB.setVisibility(View.GONE);
+                ArrayList<Articles> articles = newsModal.getArticles();
+                for (int i = 0; i < articles.size(); i++) {
+                    articlesArrayList.add(new Articles(articles.get(i).getTitle(),
+                            articles.get(i).getDescription(),articles.get(i).getUrlToImage(),
+                            articles.get(i).getUrl(),articles.get(i).getContent()));
+                }
+                newsRVAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(Call<NewsModal> call, Throwable t) {
+                Toast.makeText(HomeFragment.this.getContext(), "Error al presentar las noticias", Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 
     @Override

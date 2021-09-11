213756e603dd34f084ffe88585f0c8ea24c2ad48
Wilson-CV <carpiowilson00@outlook.com>
2021-09-10 20:06:07 -0500

    Modificar NewsDeatilActivity.java

--- app/src/main/java/com/my/first/taller_sharedpreference/NewsDeatilActivity.java
+++ app/src/main/java/com/my/first/taller_sharedpreference/NewsDeatilActivity.java
@@ -2,10 +2,22 @@ package com.my.first.taller_sharedpreference;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class NewsDeatilActivity extends AppCompatActivity {
     String title, content, desc, imageURL,url;
+    private TextView titleTV, subDescTV, contentTV;
+    private ImageView newsIV;
+    private Button readNewsBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +27,23 @@ public class NewsDeatilActivity extends AppCompatActivity {
         desc = getIntent().getStringExtra("desc");
         imageURL = getIntent().getStringExtra("image");
         url = getIntent().getStringExtra("url");
+        titleTV = findViewById(R.id.idTVTitle);
+        subDescTV = findViewById(R.id.idTVSubDesc);
+        contentTV = findViewById(R.id.idTVContent);
+        newsIV = findViewById(R.id.idIVNews);
+        readNewsBtn = findViewById(R.id.idBtnReadNews);
+
+        titleTV.setText(title);
+        subDescTV.setText(desc);
+        contentTV.setText(content);
+        Picasso.get().load(imageURL).into(newsIV);
+        readNewsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_SEND);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
     }
 }
